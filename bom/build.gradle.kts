5457db01a2259d765ff8d6e18cb887d2e9fa67bc
Jack Berg <jberg@newrelic.com>
2025-06-26 14:27:50 -0500

    demo

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -7,3 +7,10 @@ group = "io.opentelemetry"
 base.archivesName.set("opentelemetry-bom")
 
 otelBom.projectFilter.set { !it.hasProperty("otel.release") }
+
+javaPlatform.allowDependencies()
+
+dependencies {
+  // Add dependency on opentelemetry-bom to ensure synchronization between alpha and stable artifacts
+  api(platform("org.springframework.boot:spring-boot-dependencies:3.5.3"))
+}
